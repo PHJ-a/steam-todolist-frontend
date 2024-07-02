633c761d5021d8233db7a54871c6e4606b215e6e
minseok <imuchgabis@gmail.com>
2024-07-02 20:03:55 +0900

    #1 feat : 리액트 라우터 설정, 레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './style/global';
+import Layout from './layout/Layout';
+import Home from './pages/Home';
+import CreateTodo from './pages/CreateTodo';
 
 const App = () => {
   return (
     <div>
       <GlobalStyle />
-      App
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index path='/' element={<Home />} />
+            <Route path='/create' element={<CreateTodo />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <div id='modalRoot' />
     </div>
   );
 };
