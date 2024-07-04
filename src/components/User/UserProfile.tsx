a63ebd0e43d00a7d5abd3be96ad9e069ccd3927b
minseok <imuchgabis@gmail.com>
2024-07-04 17:47:57 +0900

    #20 팝오버 컴포넌트, 로그아웃 구현

--- src/components/User/UserProfile.tsx
+++ src/components/User/UserProfile.tsx
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 import logo from '../../assets/userProfile.gif';
+import { useState } from 'react';
+import PopOver from '../common/PopOver';
 const UserProfile = () => {
+  const [open, setOpen] = useState(false);
   return (
     <StyleUserProfile>
-      <img src={logo} />
+      <img src={logo} onClick={() => setOpen((prev) => !prev)} />
+      {open && <PopOver close={() => setOpen(false)} />}
     </StyleUserProfile>
   );
 };
@@ -11,6 +15,7 @@ const UserProfile = () => {
 const StyleUserProfile = styled.div`
   margin-right: 40px;
   cursor: pointer;
+  position: relative;
 `;
 
 export default UserProfile;
---
+++ src/components/common/PopOver.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import axiosInstance from '../../api/axios';
+import { useAuth } from '../../context/AuthContext';
+
+type PopOverProps = {
+  close: () => void;
+};
+
+const PopOver = ({ close }: PopOverProps) => {
+  const popoverRef = useRef<HTMLDivElement>(null);
+  const { setIsLoggedIn } = useAuth();
+
+  const handleLogout = async () => {
+    try {
+      await axiosInstance.get('/logout');
+      setIsLoggedIn(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        popoverRef.current &&
+        !popoverRef.current.contains(e.target as Node)
+      ) {
+        close();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [close]);
+  return (
+    <StyledPopOver ref={popoverRef} onClick={(e) => e.stopPropagation()}>
+      <ul className='menus'>
+        <li onClick={handleLogout}>로그아웃</li>
+        <li>완료한 도전과제</li>
+      </ul>
+    </StyledPopOver>
+  );
+};
+
+const StyledPopOver = styled.div`
+  position: absolute;
+  z-index: 100;
+  width: 220px;
+  background-color: #fff;
+  color: black;
+  border: 1px solid #ccc;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  padding: 10px;
+  top: calc(100% + 10px);
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  .menus {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    width: 100%;
+
+    li {
+      padding: 8px 10px;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+
+      &:hover {
+        background-color: #f0f0f0;
+      }
+    }
+  }
+`;
+
+export default PopOver;
