5f1509404bc0b4ab2fa78c59013c67c4929c95a9
phj <aaagowns@gmail.com>
2024-07-08 22:12:37 +0900

    #28 Style : Header 스타일 변경
    - 프로필을 react-icon으로 변경
    - steam.png 변경

--- src/components/User/UserProfile.tsx
+++ src/components/User/UserProfile.tsx
@@ -2,20 +2,25 @@ import styled from 'styled-components';
 import logo from '../../assets/userProfile.gif';
 import { useState } from 'react';
 import PopOver from '../common/PopOver';
+import { CgProfile } from 'react-icons/cg';
 const UserProfile = () => {
   const [open, setOpen] = useState(false);
   return (
     <StyleUserProfile>
-      <img src={logo} onClick={() => setOpen((prev) => !prev)} />
+      <div className='icon'>
+        <CgProfile size={34} onClick={() => setOpen(!open)} />
+      </div>
       {open && <PopOver close={() => setOpen(false)} />}
     </StyleUserProfile>
   );
 };
 
 const StyleUserProfile = styled.div`
-  margin-right: 40px;
   cursor: pointer;
   position: relative;
+  .icon {
+    color: white;
+  }
 `;
 
 export default UserProfile;
