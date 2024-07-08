c88dc80b774ea8ed8928c3c658864fb190c1dd9e
phj <aaagowns@gmail.com>
2024-07-09 01:33:14 +0900

    #30 Style : empty 로고 크기 수정
    - empty로고 크기를 png 크기로 변경하기 위해 logo 스타일 삭제

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -4,7 +4,9 @@ import icon from '../assets/Empty.png';
 const Empty = ({ onLoginClick }: { onLoginClick: () => void }) => {
   return (
     <EmptyStateContainer>
-      <img className='logo' src={icon} alt='Logo' />
+      <div className='img'>
+        <img src={icon} alt='empty' className='logo' />
+      </div>
       <p className='message'>로그인 후 서비스 이용가능하십니다 </p>
       <p>달력을 누르면 도전과제의 상세정보를 볼수 있습니다</p>
       <button className='loginBtn' onClick={onLoginClick}>
@@ -18,20 +20,17 @@ const EmptyStateContainer = styled.div`
   flex: 3;
   display: flex;
   flex-direction: column;
-
+  justify-content: center;
   align-items: center;
   height: 100%;
   background-color: #f2f2f2;
-  .logo {
-    width: 300px;
-    height: 300px;
-    margin-bottom: 20px;
-  }
+
   .message {
     font-weight: 700;
     font-size: 30px;
     margin-bottom: 1rem;
   }
+
   .loginBtn {
     width: 200px;
     border-radius: 20px;
