ae5e6bec29d1b6e8fa443b3bc0b9096c86282775
phj <aaagowns@gmail.com>
2024-07-12 14:19:55 +0900

    feat : .env 추가
    - https 개발환경 추가
    - env 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://steam-todo.ap-northeast-2.elasticbeanstalk.com/';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    // https 환경에서 개발할 때 사용
+    // basicSsl({
+    //   domains: ['api.todos-steams-project.site', 'todos-steams-project.site'],
+    // }),
+  ],
+});
