bb328a3c50fd33d78dab9dd2120687f90bb65b25
phj <aaagowns@gmail.com>
2024-07-15 17:07:59 +0900

    언락타임 포매팅 추가

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -10,3 +10,15 @@ export const formatLastPlayed = (lastPlayedTime: string) => {
   const day = date.getDate().toString().padStart(2, '0');
   return `${year}년 ${month}월 ${day}일`;
 };
+
+export const formatUnlockTime = (unlockTime: string) => {
+  const date = new Date(unlockTime);
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours();
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const period = hours >= 12 ? '오후' : '오전';
+  const formattedHours = (hours % 12 || 12).toString().padStart(2, '0');
+  return `${year}년 ${month}월 ${day}일 ${period} ${formattedHours}시 ${minutes}분`;
+};
